6bcbc241089c3e8a89d6535d6f6e666996176548
Pierre GILLON <contact@pierregillon.com>
2023-04-05 11:26:06 +0200

    refactor: introduce function to move from Points to Forty and Forty to next step

--- src/TennisKata/TennisGame.fs
+++ src/TennisKata/TennisGame.fs
@@ -14,54 +14,47 @@ type FortyData =
         Player: Player
         OtherPlayerPoint: Point
     }
+    
+type PointsData = Point * Point
 
 type Score =
-    | Points of Point * Point
+    | Points of PointsData
     | Forty of FortyData
     | Deuce
     | Advantage of Player
     | Game of Player
 
-let addOnePoint point =
+let setupGame: Score = Points(Love, Love)
+
+let increasePoint point =
     match point with
     | Love -> Fifty
     | Fifty -> Thirty
     | _ -> failwith "not implemented: cannot increase point"
 
-let setupGame: Score = Points(Love, Love)
+let addOnePoint playerWinningPoint (player1Score, player2Score) =
+    if playerWinningPoint = PlayerOne && player1Score = Thirty then
+        Forty { Player = playerWinningPoint; OtherPlayerPoint = player2Score }
+    elif playerWinningPoint = PlayerTwo && player2Score = Thirty then
+        Forty { Player = playerWinningPoint; OtherPlayerPoint = player1Score }
+    elif playerWinningPoint = PlayerOne then
+        Points (increasePoint player1Score, player2Score)
+    else
+        Points (player1Score, increasePoint player2Score)
+ 
+let addOnePointAtForty playerWinningPoint forty =
+    if forty.Player = playerWinningPoint then Game playerWinningPoint
+    elif forty.OtherPlayerPoint = Thirty then Deuce
+    else Forty { forty with OtherPlayerPoint = increasePoint forty.OtherPlayerPoint }
 
 let playerWin currentScore playerWinningPoint : Score =
     match currentScore with
-    | Game _ -> failwith "already won"
+    | Points points -> addOnePoint playerWinningPoint points
+    | Forty forty -> addOnePointAtForty playerWinningPoint forty
     | Deuce -> Advantage playerWinningPoint
     | Advantage player when player = playerWinningPoint -> Game playerWinningPoint
     | Advantage _ -> Deuce
-    | Forty forty when forty.Player = playerWinningPoint -> Game playerWinningPoint
-    | Forty forty when forty.Player <> playerWinningPoint && forty.OtherPlayerPoint = Thirty -> Deuce
-    | Forty forty when forty.Player <> playerWinningPoint && forty.OtherPlayerPoint <> Thirty ->
-        Forty(
-            { forty with
-                OtherPlayerPoint = addOnePoint forty.OtherPlayerPoint
-            }
-        )
-    | Points (Thirty, player2Score) when playerWinningPoint = PlayerOne ->
-        Forty(
-            {
-                Player = playerWinningPoint
-                OtherPlayerPoint = player2Score
-            }
-        )
-    | Points (player1Score, Thirty) when playerWinningPoint = PlayerTwo ->
-        Forty(
-            {
-                Player = playerWinningPoint
-                OtherPlayerPoint = player1Score
-            }
-        )
-    | Points (player1Score, player2Score) when playerWinningPoint = PlayerOne ->
-        Points(addOnePoint player1Score, player2Score)
-    | Points (player1Score, player2Score) when playerWinningPoint <> PlayerOne ->
-        Points(player1Score, addOnePoint player2Score)
+    | Game _ -> failwith "already won"
     | _ -> failwith "invalid case"
 
 let playerOneWin score : Score = playerWin score PlayerOne
